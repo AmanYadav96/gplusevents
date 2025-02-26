2d30d2fc55e622e9bf99475d77edf4ed7b315c2b
Aman Yadav <113305877+AmanYadav96@users.noreply.github.com>
2025-02-26 18:59:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['gplusevents.liveapi.page', 'www.gplusevents.com', 'i3.ytimg.com', 'tester.shortlinker.in','13.51.238.177','13.49.137.149'],
-    unoptimized: true,
+    domains: ['gplusevents.liveapi.page', 'www.gplusevents.com', 'i3.ytimg.com', 'tester.shortlinker.in','13.51.238.177','movihome.xyz'],
+   
   },
   devIndicators: {
     buildActivity: false
